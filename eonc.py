604f4292d5d54852997825122cded605bd452f53
Mahmoud El zahabi <zahabimahmoud@gmail.com>
2025-02-12 23:40:39 +0200

    finished first program

---
+++ eonc.py
@@ -0,0 +1,6 @@
+print("\tWelcome to the even or Odd number checker")
+number = int(input("Please enter a number : "))
+if number % 2 == 0:
+    print(f"{number} is an even number")
+else:
+    print(f"{number} is an odd number")
